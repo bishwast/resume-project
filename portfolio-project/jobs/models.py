ee9574886d8a1bd0a927d59a6c3dadd4af36b55b
Sunil T Bishwakarma <bishwast77@gmail.com>
2022-05-20 11:48:54 -0600

    Database and models
    Added python class to save. a database for jobs. Installed pillow with pip3, created image and field types for Job class in models.

--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. Model is a python class that can be saved into the database.
+class Job(models.Model):
+    # Image property to save an image with a type of ImageField, this field saves images of all types.
+    image = models.ImageField(upload_to='images/')
+
+    # Summary section with a type field of Characters of max length of 200
+    summary = models.CharField(max_length=200)
