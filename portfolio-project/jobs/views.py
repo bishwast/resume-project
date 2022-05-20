96283844bb2978b1f59fbde917d2ea4f43f21b35
Sunil T Bishwakarma <bishwast77@gmail.com>
2022-05-20 13:27:31 -0600

    Contents
    Added for loop in the jobs homepage, added Job Objects to the views and updated the contents.

--- portfolio-project/jobs/views.py
+++ portfolio-project/jobs/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 # Function for sunil view paths. This fun will render htmm code to the user using render.
 def home(request):
-    return render(request, 'jobs/home.html')
+    # Adding Job objects from the django database to the views
+    jobs = Job.objects
+    # Added list of jobs above to the views to render jobs info
+    return render(request, 'jobs/home.html', {'jobs': jobs})
